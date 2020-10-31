ad1158ababfdc59eeff92e8e125dec360a192b17
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2020-11-01 07:40:07 +0800

    exercise 2 course 1

---
+++ Exercise2-Question.py
@@ -0,0 +1,37 @@
+# GRADED FUNCTION: train_mnist
+def train_mnist():
+    # Please write your code only where you are indicated.
+    # please do not remove # model fitting inline comments.
+
+    # YOUR CODE SHOULD START HERE
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if (logs.get('acc') > 0.99):
+                print("\nReached 99% accuracy so cancelling training!")
+                self.model.stop_training = True
+            # YOUR CODE SHOULD END HERE
+
+    callbacks = myCallback()
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path=path)
+    # YOUR CODE SHOULD START HERE
+    training_images = x_train / 255.0
+    test_images = x_test / 255.0
+    # YOUR CODE SHOULD END HERE
+    model = tf.keras.models.Sequential([
+        # YOUR CODE SHOULD START HERE
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # model fitting
+    history = model.fit(training_images, y_train, epochs=10, callbacks=[callbacks])
+    # model fitting
+    return history.epoch, history.history['acc'][-1]
